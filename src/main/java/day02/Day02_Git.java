d47b635658454f858dccd28d988ab2a91a4d6cf1
maryamdincher <maryamdincher@gmail.com>
2024-03-10 09:25:58 -0600

    v2

--- src/main/java/day02/Day02_Git.java
+++ src/main/java/day02/Day02_Git.java
@@ -3,5 +3,9 @@ package day02;
 public class Day02_Git {
     public static void main(String[] args) {
         System.out.println("Good Lock");
+
+        // version two
+        System.out.println("you can do it");
+        cls
     }
 }

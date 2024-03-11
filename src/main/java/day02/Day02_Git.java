3eb4d3d3298588da21c88b650a45bf0cb8e797d7
maryamdincher <maryamdincher@gmail.com>
2024-03-11 08:54:22 -0600

    hhg

--- src/main/java/day02/Day02_Git.java
+++ src/main/java/day02/Day02_Git.java
@@ -4,7 +4,11 @@ public class Day02_Git {
     public static void main(String[] args) {
         System.out.println("Good Lock");
 
-        // version two
+        //git remote set-url origin
+        https://github.com/maryamdincher/GitProJace.git
+
+
+      //token--  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
         System.out.println("you can do it");
 // version 3
         System.out.println("maryam");
